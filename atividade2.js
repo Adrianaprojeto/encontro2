c2c885d4c0da889534f202319a197cb64fcaf16d
Adriana <adriana.oliversilva@gmail.com>
2022-08-19 19:02:32 -0300

    PRIMEIRA VERSAO, PRIMEIRA LINHA

---
+++ atividade2.js
@@ -0,0 +1,23 @@
+if (data < 07/07/2022) {
+    console.log ("Evento criado sucesso". data);
+} else {console.log  (" Data inválida");
+}
+console.log ("Insira sua idade:");
+var idade = 18 
+if (idade>=18){ 
+    console.log (" Bem Vindo ")
+} else {
+    console.log (" Não permitido para menor de 18 anos")
+}
+
+let numeroDeAluno = 100
+let listaDeAllunos = [ "Ana"," Gustavo", "Rodolfo"," Amanda "]
+
+for (let contador = 0; contador < numeroDeAluno; contador++) {
+    console.log (contador);
+}
+
+if (contador == 100 ) {
+    console.log ("Numero igual a Zero ")
+}
+console.log ("Número atual de Pessoas Colocadas na lista:");
